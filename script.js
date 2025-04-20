3615d24baffc6caac8e952501845c7a58750bc1f
Shivam2383 <shivamvkumar2007@gmail.com>
2025-04-20 20:50:16 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+function hamburg() {
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+
+function cancel() {
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+// Typewriter Effect
+const texts = [
+    "TEACHER",
+    "LEADER",
+    "YOU TUBER"
+]
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let charcterIndex = 0;
+
+function typeWriter() {
+    if (charcterIndex < texts[textIndex].length) {
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(eraseText, 1000)
+    }
+}
+
+function eraseText() {
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50)
+    } else {
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter, 500)
+    }
+}
+window.onload = typeWriter // Burger Menu Toggle
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    // Toggle navigation visibility
+    navLinks.classList.toggle('nav-active');
+
+    // Animate nav links (staggered effect)
+    navLinks.querySelectorAll('li').forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+
+    // Burger animation toggle
+    burger.classList.toggle('toggle');
+});
